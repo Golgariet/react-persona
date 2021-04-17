d18c31d1a7fd149da537114b41b32c23af87d6e1
Anton Bibergal <nathgilt@gmail.com>
2021-04-17 19:18:50 +0300

    svg fixes

--- src/components/Home.js
+++ src/components/Home.js
@@ -27,22 +27,20 @@ function Home() {
       </section>
       <section className="section3">
         <div className="accolades_container">
-          <div className="accolades_container">
-            <div className="accolades">
-              <svg className="feature__icon">
-                <use xlinkHref="./assets/icons/trophy.svg#icon-trophy"></use>
-              </svg>
-            </div>
-            <div className="accolades">
-              <svg className="feature__icon">
-                <use xlinkHref="./assets/icons/awareness-ribbon.svg#icon-ribbon"></use>
-              </svg>
-            </div>
-            <div className="accolades">
-              <svg className="feature__icon">
-                <use xlinkHref="./assets/icons/game-controller.svg#icon-controller"></use>
-              </svg>
-            </div>
+          <div className="accolades">
+            <svg className="accolade">
+              <use xlinkHref="./assets/icons/trophy.svg#icon-trophy"></use>
+            </svg>
+          </div>
+          <div className="accolades">
+            <svg className="accolade">
+              <use xlinkHref="./assets/icons/awareness-ribbon.svg#icon-ribbon"></use>
+            </svg>
+          </div>
+          <div className="accolades">
+            <svg className="accolade">
+              <use xlinkHref="./assets/icons/game-controller.svg#icon-controller"></use>
+            </svg>
           </div>
         </div>
       </section>
@@ -62,13 +60,13 @@ function Home() {
             <ul>
               <li>
                 Telephone
-                <svg className="feature__icon">
+                <svg className="icon">
                   <use xlinkHref="./assets/icons/phone.svg#icon-phone"></use>
                 </svg>
               </li>
               <li>
                 Email
-                <svg className="feature__icon">
+                <svg className="icon">
                   <use xlinkHref="./assets/icons/mail.svg#icon-mail"></use>
                 </svg>
               </li>
@@ -78,19 +76,19 @@ function Home() {
             <ul>
               <li>
                 Instagram
-                <svg className="feature__icon">
+                <svg className="icon">
                   <use xlinkHref="./assets/icons/instagram-with-circle.svg#icon-insta"></use>
                 </svg>
               </li>
               <li>
                 Twitter
-                <svg className="feature__icon">
+                <svg className="icon">
                   <use xlinkHref="./assets/icons/twitter.svg#icon-twitter"></use>
                 </svg>
               </li>
               <li>
                 Facebook
-                <svg className="feature__icon">
+                <svg className="icon">
                   <use xlinkHref="./assets/icons/facebook.svg#icon-facebook"></use>
                 </svg>
               </li>
